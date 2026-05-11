483192530893c75492f01c11fc96dc0eddba74d1
Ameer <ameer.alkhazraji96@gmail.com>
2026-05-11 19:44:51 +0200

    change form name and orginize folder structure

---
+++ MainForm/MainForm.vb
@@ -0,0 +1,5 @@
+﻿Public Class MainForm
+    Private Sub Form_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Console.WriteLine("Hello World!")
+    End Sub
+End Class
--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.FindusUAV.Form1
+            Me.MainForm = Global.FindusUAV.MainForm
         End Sub
     End Class
 End Namespace
